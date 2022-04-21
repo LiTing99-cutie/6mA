6ada811c0d2cb07a02bd5753b37fb300a8da20cb
LiTing99-cutie <2991639975@qq.com>
2022-04-21 19:31:13 +0800

    try

---
+++ bin/test.update.sh
@@ -0,0 +1,12 @@
+################################################
+#File Name: run/test.sh
+#Author: LiTing
+#Mail: liting@stu.pku.edu.cn
+#Created Time: Thu 21 Apr 2022 07:18:01 PM CST
+################################################
+
+#!/bin/sh 
+
+set -eou pipefail
+echo "hello,world"
+echo "today is a beautiful day"
